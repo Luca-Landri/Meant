2cd2b8b9c4493948b057dccfdb42a81792b5f89d
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-07-09 19:46:36 +0200

    created .env (hide variables)

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -4,12 +4,12 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEzrYPHyPBMhDofZ_26M36lzE66hQ3FBI",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "landrigram.firebaseapp.com",
   projectId: "landrigram",
   storageBucket: "landrigram.appspot.com",
   messagingSenderId: "1011335996647",
-  appId: "1:1011335996647:web:8cf6ca38665dc205f16e42",
+  appId: import.meta.env.VITE_APP_ID,
   measurementId: "G-XCPBGY4VZD"
 };
 
