775d7b6d4423bc2942a0374a257cf3a6c53dba0b
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-07-12 16:23:30 +0200

    Completely changed the logic of google singIn
    now the google sing in is the only avaible and work correctly

--- src/app/Data.js
+++ src/app/Data.js
@@ -1,34 +1,73 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import userDefault from '../../assets/user.jpg'
+import { provider } from '../firebase/FirebaseConfig'
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { toast } from 'react-toastify';
+
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+provider.setCustomParameters({
+  'login_hint': 'user@example.com'
+});
 
 const initialState = {
   name: "",
   email: "",
   password: "",
-  img: "https://icon-library.com/images/default-user-icon/default-user-icon-13.jpg",
+  confirmPassword: "",
+  img: userDefault,
+  isAuth: false,
 }
+
+export const signInGoogle = createAsyncThunk('signInGoogle', async () => {
+    return signInWithPopup(getAuth(), provider).then(res => {
+      return res.user
+    }).catch (err => {
+      toast.error(err.message)
+    })
+})
+
+export const NormalSingIn = createAsyncThunk('NormalSingIn', async (email, password) => {
+    return signInWithEmailAndPassword(getAuth(), email, password).then(res => {
+      return res.user
+    }).catch (err => {
+      toast.error(err.message)
+    })
+})
 
 export const dataSlice = createSlice({
   name: 'data',
   initialState,
   reducers: {
-    setName: (state, action) => {
-      state.name = action.payload
-    },
-
-    setEmail: (state, action) => {
-        state.email = action.payload
-    },
-
-    setPassword: (state, action) => {
-        state.password = action.payload
-    },
-
-    setImg: (state, action) => {
-      state.img = action.payload
-    }
 
   },
+
+  extraReducers: (builder) => {
+    builder.addCase(signInGoogle.fulfilled, (state, action) => {
+        state.name = action.payload.displayName
+        state.img = action.payload.photoURL
+        state.isAuth = true
+        state.email = action.payload.email
+        console.log(state.email)
+        console.log(state.isAuth)
+    }),
+    builder.addCase(signInGoogle.rejected, (state, action) => {
+        toast.error("Login failed, try a different account")
+        state.isAuth = false
+    }),
+
+    builder.addCase(NormalSingIn.fulfilled, (state, action) => {
+        state.isAuth = true
+        state.email = action.payload.email
+        console.log(state.email)
+        console.log(state.isAuth)
+        state.password = action.payload.password
+    }),
+
+    builder.addCase(NormalSingIn.rejected, (state, action) => {
+        toast.error("Login failed, try a different account")
+        state.isAuth = false
+    })
+  } // end of extraReducers
 })
 
 export default dataSlice.reducer
-export const { setPassword, setEmail, setImg, setName } = dataSlice.actions
