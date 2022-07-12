67bf6299676aaf2755220e9c7f336d2dde240343
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-07-12 16:24:55 +0200

    Added cookie provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/Store";
 import { Provider } from 'react-redux'
+import { CookiesProvider } from "react-cookie";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <CookiesProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </CookiesProvider>
   </React.StrictMode>
 )
