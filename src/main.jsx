ffdc023f3febdf8f90d93d1c97628b3386134055
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-07-13 13:37:30 +0200

    Removed the conteiner of "react-cookie"

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,13 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/Store";
 import { Provider } from 'react-redux'
-import { CookiesProvider } from "react-cookie";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CookiesProvider>
       <Provider store={store}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </Provider>
-    </CookiesProvider>
   </React.StrictMode>
 )
