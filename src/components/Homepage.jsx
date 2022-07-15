eda192529dc8586a51fe8884681bca6bc11f23c9
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-07-15 10:39:44 +0200

    Trying to take the images from the storage
    Now it's not possible having the image but we have only the path

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import { read_cookie, delete_cookie } from 'sfcookies';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { getStorage, ref, getDownloadURL, listAll  } from "firebase/storage";
 
 const Title = styled.h1`
     font-size: 30px;
@@ -14,7 +15,6 @@ const Title = styled.h1`
     font-weight: 600;
     color: #ffffff;
     text-align: center;
-
 `
 
 
@@ -23,13 +23,40 @@ const Homepage = () => {
     const dispatch = useDispatch()
     const nameCookie = read_cookie("name")
     const navigate = useNavigate()
+    const storage = getStorage();
+    const starsRef = ref(storage, 'images/');
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        listAll(starsRef).then(res => {
+            res.items.forEach((itemRef) => {
+                setImages(prevState => [...prevState, itemRef._location.path])
+            });
+        }).catch(err => {
+            console.log(err)
+        })
+
+        console.log(images)
+    }, [])
+    
+    const showImages = () => {
+        console.log(images)
+    }
 
     return (
         <div>
             <Header/>
-            <Title>HI {nameCookie}</Title>
+            <Title onClick={showImages}>HI {nameCookie}</Title>
 
             { openForm ? <UploadForm/> : null }
+
+            <div>
+                {images.map((image, index) => {
+                    return (
+                        <div>{image}</div>
+                    )
+                })}
+            </div>
             
             <Navbar/>
         </div>
