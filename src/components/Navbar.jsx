ab2f85f4a6f206c7d6bb7ff12afc64233b6f7a3d
Luca <73708474+Luca-Landri@users.noreply.github.com>
2022-07-15 09:51:56 +0200

    Navbar now is better designed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,30 @@ import { Icon } from '@iconify/react';
 import { useSelector, useDispatch } from 'react-redux'
 import { setFormOpened } from '../app/app';
 
+const NavbarIcons = [
+    {
+        id: 1,
+        icon: "ant-design:home-filled",
+        name: "Home",
+    },
+    {
+        id: 2,
+        icon: "carbon:add-filled",
+        name: "Upload",
+    },
+    {
+        id: 3,
+        icon: "bx:user",
+        name: "Profile",
+    },
+    {
+        id: 4,
+        icon: "bx:cog",
+        name: "Settings",
+    }
+]
+
+
 const Container = styled.div`
     position: absolute;
     bottom: 15px;
@@ -14,7 +38,7 @@ const Container = styled.div`
 `
 
 const NavbarRow = styled.div`
-    background-color: purple;
+    background-color: #5E5DF0;
     width: 85%;
     border-radius: 30px;
     display: flex;
@@ -22,8 +46,22 @@ const NavbarRow = styled.div`
     justify-content: center;
     margin-left: auto;
     margin-right: auto;
-    gap: 55px;
+    gap: 45px;
     padding: 10px;
+`
+
+const Name = styled.div`
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    color: white;
+`
+
+const Option = styled.div`
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
 `
 
 
@@ -36,9 +74,26 @@ const Navbar = () => {
     return (
         <Container>
             <NavbarRow>
-                <Icon icon="ant-design:home-filled" color="white" width="34" height="34" />
-                <Icon icon="carbon:add-filled" color="white" width="34" height="34" onClick={() => dispatch(setFormOpened(formOpened))}/>
-                <Icon icon="bx:user" color="white" width="34" height="34"/>
+                {NavbarIcons.map((icon) => {
+                    return (
+                        <Option>
+                            <Icon
+                            color="white"
+                            width="34"
+                            key={icon.id}
+                            icon={icon.icon}
+                            onClick={() => {
+                                if (icon.name === "Upload") {
+                                    dispatch(setFormOpened())
+                                }
+                            }
+                            }
+                            />
+                            <Name>{icon.name}</Name>
+                        </Option>
+                        
+                    )
+                })}
             </NavbarRow>
         </Container>
     )
